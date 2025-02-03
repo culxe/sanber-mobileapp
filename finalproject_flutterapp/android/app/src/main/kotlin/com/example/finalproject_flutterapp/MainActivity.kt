59d36bec989e2261e6f5a11d23b6cdfbf6df2f00
Indiana Namaul <91829683+culxe@users.noreply.github.com>
2025-02-03 15:49:03 +0700

    delete yg error

--- finalproject_flutterapp/android/app/src/main/kotlin/com/example/finalproject_flutterapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.finalproject_flutterapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- finalproject_flutterapp/lib/routes/app_routes_named.dart
+++
@@ -1,7 +0,0 @@
-class AppRoutesNamed {
-  static const splashScreen = '/';
-  static const getStartedPage = '/getStarted';
-  static const loginPage = '/login-page';
-  static const registerPage = '/register-page';
-  static const homeScreen = '/home-screen';
-}
